4e778aacb5bd9d47d843f098609368bf8332e5e1
kennethferdinandd <kennethferdinand03@gmail.com>
2022-09-07 22:03:20 +0700

    Menambahkan Lab 1 fix

--- wishlist/views.py
+++ wishlist/views.py
@@ -3,9 +3,10 @@ from wishlist.models import BarangWishlist
 
 # Create your views here.
 def show_wishlist(request):
-    data_barang_wishlist = BarangWishlist.objects.all()
-    context = {
+    return render(request, "wishlist.html", context)
+
+data_barang_wishlist = BarangWishlist.objects.all()
+context = {
         'list_barang': data_barang_wishlist,
         'nama': 'Kenneth'
     }
-    return render(request, "wishlist.html", context)
